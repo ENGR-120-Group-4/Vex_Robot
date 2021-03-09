8748ebb6ee0b6e96da733cee4274b37472470a78
CaelumD25 <caelumdudek25@gmail.com>
2021-03-09 00:25:08 -0800

    Adding our starting code!

---
+++ G04_VEX.c
@@ -0,0 +1,34 @@
+#pragma config(Sensor, inl, irSensor, sensorReflection)
+#pragma config(Sensor, dgt11, yMax, sensorTouch)
+#pragma config(Sensor, dgt12, yMin, sensorTouch)
+#pragma config(Sensor, dgt13, xMin, sensorTouch)
+#pragma config(Sensor, dgt14, xMin, sensorTouch)
+#pragma config(Motor, port2, xMotor, tmotorServoContinousRotation, openLoop)
+#pragma config(Motor, port3, yMotor, tmotorServoContinousRotation, openLoop)
+
+
+// Global Variables to update for the storing of locations of the ir sensor
+long xpos = 0;
+long ypos = 0;
+
+// Just moves the robot arm left and right
+// This code should not be used in the future, this is just for demonstration
+int motor_x(int max, int min){
+  if (max!=0||min!=0) {
+    motor[xMotor] = motor[xMotor]*-1;
+    return motor[xMotor];
+  }
+  else{
+    return motor[xMotor];
+  }
+}
+
+
+task main(){
+  motor[xMotor] = 100;
+  while(1){
+    motor[xMotor] = motor_x(SensorValue[xMax],SensorValue[xMin])
+    wait1Msec(1000);
+  }
+
+}
